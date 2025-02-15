de7460f3540b11022c798ee8736a819df4e34207
Тимофій Лейцишин <145194518+leitsyshyn@users.noreply.github.com>
2025-02-15 20:31:15 +0200

    Set up Auth.js

---
+++ src/lib/auth.ts
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "./prisma";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        return null;
+      },
+    }),
+  ],
+});
